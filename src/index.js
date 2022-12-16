063b0caea066692835b5a751577813a6e4a7a789
Bilogolovyi Sergii <sergey.hotchili@gmail.com>
2022-12-16 20:53:30 +0800

    added files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Notiflix from 'notiflix';
+import 'regenerator-runtime/runtime';
+import axios from 'axios';
+import SimpleLightbox from "simplelightbox";
+
+const API_KEY =`32122100-8cc25e477ccf1dfc443f6b4e8`;
+const BASE_URL = `https://pixabay.com/api/`;
